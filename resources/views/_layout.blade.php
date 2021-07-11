cb5c77a6ee01546805972198c14d6a0d7ef2635e
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-07-10 22:28:54 -0300

    [DEV] - Include Font awesome

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dailytasks - @yield('title')</title>
     <link rel="stylesheet" href="{{asset('site/bootstrap.css')}}">
+    <script src="https://kit.fontawesome.com/d5e0d5b45e.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <main class="container">@yield('content')</main>
