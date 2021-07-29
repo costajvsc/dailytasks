4ad12e3606534655d728b0d90261c31d360c37f8
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-07-28 21:05:55 -0300

    [DEV] - Improve style bootstrap on application

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/site/bootstrap.css')
+mix.sass('resources/views/scss/style.scss', 'public/site/bootstrap.css')
     .scripts('node_modules/jquery/dist/jquery.js', 'public/site/jquery.js')
     .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/site/bootstrap.js');
